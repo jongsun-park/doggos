cdcf1439a2c445e4589dffaac84973009a844b4c
Jongsun <jongsun250@gmail.com>
2022-10-19 22:57:14 +0100

    update submit form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,144 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 import RandomDogProfile from "../components/section/RandomDogProfile";
 import GradientText from "../components/ui/GradientText";
-import { doggos } from "../data/doggos";
+
+const DoggosTable = ({ data }) => {
+  if (data.length === 0) return;
+  return (
+    <TableContainer>
+      <table>
+        <thead>
+          <tr>
+            <th className="name">Name</th>
+            <th className="breed">Breed</th>
+            <th className="birth">Birth</th>
+            <th className="color">Color</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(({ name, breed, birth, color }) => {
+            return (
+              <tr key={name + breed + birth + color}>
+                <td className="name">{name}</td>
+                <td className="breed">{breed}</td>
+                <td className="birth">{birth}</td>
+                <td className="color">{color}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </TableContainer>
+  );
+};
+
+const TableContainer = styled.div`
+  width: 100%;
+
+  table {
+    width: 100%;
+  }
+  th {
+    text-align: left;
+    text-transform: uppercase;
+  }
+  td {
+    &.name {
+      text-transform: uppercase;
+    }
+  }
+`;
+
+const DoggoForm = ({ onSubmit, onChange, data, onReset }) => (
+  <form onSubmit={onSubmit}>
+    {/* Name */}
+    <label>
+      Name:
+      <input
+        type="text"
+        name="name"
+        value={data.name}
+        onChange={onChange}
+        required
+      />
+    </label>
+    <br />
+
+    {/* Bread */}
+    <label>
+      Breed:
+      <input
+        type="text"
+        name="breed"
+        value={data.breed}
+        onChange={onChange}
+        required
+      />
+    </label>
+    <br />
+
+    {/* Birth */}
+    <label>
+      Birth:
+      <input
+        type="date"
+        name="birth"
+        value={data.birth}
+        onChange={onChange}
+        required
+      />
+    </label>
+    <br />
+
+    {/* Color */}
+    <label>
+      Color:
+      <input
+        type="color"
+        name="color"
+        value={data.color}
+        onChange={onChange}
+        required
+      />
+    </label>
+
+    <div className="buttons">
+      <button type="reset" onClick={onReset}>
+        Clear Doggo
+      </button>
+      <button type="submit">Add Doggo</button>
+    </div>
+  </form>
+);
 
 const ContactPage = () => {
+  const [doggosData, setDoggosData] = useState([]);
+
+  const initDoggo = {
+    name: "",
+    breed: "",
+    birth: "2022-10-19",
+    color: "#ffffff",
+  };
+
+  const [doggoData, setDoggoData] = useState(initDoggo);
+
+  const resetDoggo = () => {
+    setDoggoData(initDoggo);
+  };
+
+  const addDoggo = (e) => {
+    e.preventDefault();
+    setDoggosData((prev) => [doggoData, ...prev]);
+    resetDoggo();
+  };
+
+  const changeDoggo = ({ target }) => {
+    setDoggoData((prev) => ({ ...prev, [target.name]: target.value }));
+  };
+
   return (
     <Container>
       <GradientText
@@ -13,31 +147,15 @@ const ContactPage = () => {
         gradient="0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%"
       />
       <RandomDogProfile />
-      <form>
-        <input placeholder="sample 1" type="text" />
-        <input placeholder="sample 2" type="text" />
-        <input placeholder="sample 3" type="text" />
-        <button type="submit">Submit</button>
-        <button type="reset">Reset</button>
-      </form>
-      <hr />
-      <table>
-        <thead>
-          <tr>
-            <th>TH1</th>
-            <th>TH2</th>
-            <th>TH3</th>
-          </tr>
-        </thead>
+      <DoggoForm
+        data={doggoData}
+        onChange={changeDoggo}
+        onSubmit={addDoggo}
+        onReset={resetDoggo}
+      />
 
-        <tbody>
-          <tr>
-            <td>TR1</td>
-            <td>TR2</td>
-            <td>TR3</td>
-          </tr>
-        </tbody>
-      </table>
+      <hr />
+      <DoggosTable data={doggosData} />
     </Container>
   );
 };
